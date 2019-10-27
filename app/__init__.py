c96cfe9ae4f9b603e3821a645ba5e8a319341a96
hinesw57 <Hinesw57@gmail.com>
2019-10-27 11:02:11 -0400

    corrected congif error

--- app/__init__.py
+++ app/__init__.py
@@ -62,7 +62,7 @@ def create_app(config_class=Config):
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
 
-        if app.confit['LOG_TO_STDOUT']:
+        if app.config['LOG_TO_STDOUT']:
             stream_handler = logging.StreamHandler()
             stream_handler.setLevel(logging.INFO)
             app.logger.addHandler(stream_handler)
